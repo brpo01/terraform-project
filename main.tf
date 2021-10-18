2441ab0a94ddd17a73b2c533ac916c52ca3f8331
brpo01 <opraise139@gmail.com>
2021-10-18 21:35:18 +0100

    created vpc resource

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 # Create VPC
-// resource "aws_vpc" "main" {
-//   cidr_block                     = "172.16.0.0/16"
-//   enable_dns_support             = "true"
-//   enable_dns_hostnames           = "true"
-//   enable_classiclink             = "false"
-//   enable_classiclink_dns_support = "false"
-// }
+resource "aws_vpc" "main" {
+  cidr_block                     = "172.16.0.0/16"
+  enable_dns_support             = "true"
+  enable_dns_hostnames           = "true"
+  enable_classiclink             = "false"
+  enable_classiclink_dns_support = "false"
+}
