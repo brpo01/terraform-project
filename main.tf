6114d4b188925af83874d936fdf80f91de11e757
brpo01 <opraise139@gmail.com>
2021-10-18 23:06:15 +0100

    created a variables file and added all the variables

--- main.tf
+++ main.tf
@@ -3,34 +3,6 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 
-variable "region" {
-  default = "us-west-2"
-}
-
-variable "vpc_cidr" {
-  default = "172.16.0.0/16"
-}
-
-variable "enable_dns_support" {
-  default = "true"
-}
-
-variable "enable_dns_hostnames" {
-  default = "true"
-}
-
-variable "enable_classiclink" {
-  default = "false"
-}
-
-variable "enable_classiclink_dns_support" {
-  default = "false"
-}
-
-variable "preferred_number_of_public_subnets" {
-  default = 2
-}
-
 provider "aws" {
   region = var.region
 }
