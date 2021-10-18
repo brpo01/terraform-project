657c349d8fda0f56faf49b574f2c8007d69ab2fe
brpo01 <opraise139@gmail.com>
2021-10-18 22:09:16 +0100

    created variables for vpc arguments

--- main.tf
+++ main.tf
@@ -2,6 +2,26 @@ variable "region" {
     default = "us-west-2"
 }
 
+variable "vpc_cidr" {
+    default = "172.16.0.0/16"
+}
+
+variable "enable_dns_support" {
+    default = "true"
+}
+
+variable "enable_dns_hostnames" {
+    default ="true" 
+}
+
+variable "enable_classiclink" {
+    default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+    default = "false"
+}
+
 provider "aws" {
   region = var.region
 }
