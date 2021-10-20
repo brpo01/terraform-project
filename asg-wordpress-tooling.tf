604134397792b332b6e5d50c841d6adf26238b22
brpo01 <opraise139@gmail.com>
2021-10-21 00:50:34 +0100

    created autoscaling group for wordpress application

--- asg-wordpress-tooling.tf
+++ asg-wordpress-tooling.tf
@@ -34,3 +34,28 @@ resource "aws_launch_template" "wordpress-launch-template" {
   user_data = filebase64("${path.module}/wordpress.sh")
 }
 
+# ---- Autoscaling for wordpress application
+
+resource "aws_autoscaling_group" "wordpress-asg" {
+  name                      = "wordpress-asg"
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  vpc_zone_identifier = [
+
+    aws_subnet.private[0].id,
+    aws_subnet.private[1].id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.wordpress-launch-template.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "wordpress-asg"
+    propagate_at_launch = true
+  }
+}
