54efe890daff1b6420cc5b2b6489b60280e95123
brpo01 <opraise139@gmail.com>
2021-10-20 15:26:21 +0100

    created an iam assume role policy resource to give ec2 instances temporary access to aws resources

--- roles.tf
+++ roles.tf
@@ -22,3 +22,28 @@ resource "aws_iam_role" "ec2_instance_role" {
   )
 }
 
+resource "aws_iam_policy" "policy" {
+  name        = "ec2_instance_policy"
+  description = "A test policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name =  "aws assume policy"
+    },
+  )
+
+}
