879e062b9e0ce6f6014a7c68e1bc7134d90e4445
brpo01 <opraise139@gmail.com>
2021-10-20 15:25:06 +0100

    created the iam assumerole resource

---
+++ roles.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "ec2_instance_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "aws assume role"
+    },
+  )
+}
+
