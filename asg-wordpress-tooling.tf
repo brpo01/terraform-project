724f2511ff3af01cf3fd99fd7b3cd77a61cb3b23
brpo01 <opraise139@gmail.com>
2021-10-21 00:54:43 +0100

    created launch template resource for tooling

--- asg-wordpress-tooling.tf
+++ asg-wordpress-tooling.tf
@@ -44,7 +44,6 @@ resource "aws_autoscaling_group" "wordpress-asg" {
   health_check_type         = "ELB"
   desired_capacity          = 1
   vpc_zone_identifier = [
-
     aws_subnet.private[0].id,
     aws_subnet.private[1].id
   ]
@@ -58,4 +57,45 @@ resource "aws_autoscaling_group" "wordpress-asg" {
     value               = "wordpress-asg"
     propagate_at_launch = true
   }
+}
+
+# attaching autoscaling group of  wordpress application to internal loadbalancer
+resource "aws_autoscaling_attachment" "asg_attachment_wordpress" {
+  autoscaling_group_name = aws_autoscaling_group.wordpress-asg.id
+  alb_target_group_arn   = aws_lb_target_group.wordpress-tgt.arn
+}
+
+# launch template for toooling
+resource "aws_launch_template" "tooling-launch-template" {
+  image_id               = var.ami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ip.id
+  }
+
+  key_name = var.keypair
+
+  placement {
+    availability_zone = "random_shuffle.az_list.result"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "tooling-launch-template"
+    },
+  )
+
+  }
+
+  user_data = filebase64("${path.module}/tooling.sh")
 }
