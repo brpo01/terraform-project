154eb551ad9b3c9474067f19a7b9cb9d9f691453
brpo01 <opraise139@gmail.com>
2021-10-20 15:29:41 +0100

    created an iam instance profile resource

--- roles.tf
+++ roles.tf
@@ -52,3 +52,8 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
     role       = aws_iam_role.ec2_instance_role.name
     policy_arn = aws_iam_policy.policy.arn
 }
+
+resource "aws_iam_instance_profile" "ip" {
+    name = "aws_instance_profile_test"
+    role =  aws_iam_role.ec2_instance_role.name
+}
