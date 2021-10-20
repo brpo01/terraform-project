fc637d7977d2de9d02d135608ed47924de2553f1
brpo01 <opraise139@gmail.com>
2021-10-20 15:27:35 +0100

    attach iam role to iam policy

--- roles.tf
+++ roles.tf
@@ -47,3 +47,8 @@ resource "aws_iam_policy" "policy" {
   )
 
 }
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+    role       = aws_iam_role.ec2_instance_role.name
+    policy_arn = aws_iam_policy.policy.arn
+}
