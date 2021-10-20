312532a83887e69a1d0c140c046343bdf7b399f1
brpo01 <opraise139@gmail.com>
2021-10-20 15:08:39 +0100

    created a resource for associating the public route table with the public subnets

--- route_tables.tf
+++ route_tables.tf
@@ -16,3 +16,22 @@ resource "aws_route_table_association" "private-subnets-assoc" {
   subnet_id      = element(aws_subnet.private_subnet[*].id, count.index)
   route_table_id = aws_route_table.private-rtb.id
 }
+
+# create route table for the public subnets
+resource "aws_route_table" "public-rtb" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-Public-Route-Table", var.name)
+    },
+  )
+}
+
+# associate all public subnets to the public route table
+resource "aws_route_table_association" "public-subnets-assoc" {
+  count          = length(aws_subnet.public[*].id)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public-rtb.id
+}
