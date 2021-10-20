0c5b7d9c113c85422d043cb12a9644ae90cc542d
brpo01 <opraise139@gmail.com>
2021-10-20 14:55:42 +0100

    changed igw alias

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -1,4 +1,4 @@
-resource "aws_internet_gateway" "main-igw" {
+resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(
--- natgateway.tf
+++ natgateway.tf
@@ -1,11 +1,12 @@
+# Elastic ip resource
 resource "aws_eip" "nat_eip" {
   vpc        = true
-  depends_on = [aws_internet_gateway.ig]
+  depends_on = [aws_internet_gateway.main_igw.id]
 
   tags = merge(
     var.tags,
     {
-      Name = format("%s-EIP", var.name)
+      Name = format("%s-EIP", var.environment)
     },
   )
 }
@@ -13,12 +14,12 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = element(aws_subnet.public.*.id, 0)
-  depends_on    = [aws_internet_gateway.ig]
+  depends_on    = [aws_internet_gateway.main_igw.id]
 
   tags = merge(
     var.tags,
     {
-      Name = format("%s-Nat", var.name)
+      Name = format("%s-Nat", var.environment)
     },
   )
 }
