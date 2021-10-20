fe8bfb019055b65684c6922c093bd52d64a69dc8
brpo01 <opraise139@gmail.com>
2021-10-20 13:56:18 +0100

    created private subnet resource

--- main.tf
+++ main.tf
@@ -17,11 +17,19 @@ resource "aws_vpc" "main" {
 }
 
 # Create public subnets1
-resource "aws_subnet" "public1" {
+resource "aws_subnet" "public_subnet" {
   count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = var.preferred_number_of_private_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_private_subnets
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index+2)
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
--- variables.tf
+++ variables.tf
@@ -26,6 +26,10 @@ variable "preferred_number_of_public_subnets" {
   default = 2
 }
 
+variable "preferred_number_of_private_subnets" {
+  default = 4
+}
+
 variable "tags" {
   description = "A mapping of tags to assign to all resources."
   type        = map(string)
