5bfafc56bf692590f91b21ac1169dc6cdaa9e9dd
brpo01 <opraise139@gmail.com>
2021-10-20 21:54:53 +0100

    created int lb listeners

--- alb.tf
+++ alb.tf
@@ -116,3 +116,36 @@ resource "aws_lb_target_group" "tooling-tgt" {
   target_type = "instance"
   vpc_id      = aws_vpc.main.id
 }
+
+# For this aspect a single listener was created for the wordpress which is default,
+# A rule was created to route traffic to tooling when the host header changes
+
+resource "aws_lb_listener" "web-listener" {
+  load_balancer_arn = aws_lb.ialb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate_validation.oyindamola.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress-tgt.arn
+  }
+}
+
+# listener rule for tooling target
+
+resource "aws_lb_listener_rule" "tooling-listener" {
+  listener_arn = aws_lb_listener.web-listener.arn
+  priority     = 99
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tooling-tgt.arn
+  }
+
+  condition {
+    host_header {
+      values = ["tooling.oyindamola.gq"]
+    }
+  }
+}
