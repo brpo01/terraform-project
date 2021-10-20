b45149895dc26e12adc29b2982dd4a5af344bfc8
brpo01 <opraise139@gmail.com>
2021-10-20 21:35:21 +0100

    created the target group resource

--- ext-alb.tf
+++ ext-alb.tf
@@ -1,3 +1,4 @@
+// external loadbalancer resource to direct traffic to the public subnet
 resource "aws_lb" "ext-alb" {
   name     = "ext-alb"
   internal = false
@@ -19,4 +20,21 @@ resource "aws_lb" "ext-alb" {
 
   ip_address_type    = "ipv4"
   load_balancer_type = "application"
+}
+
+// target group resource to inform loadbalancer where to route traffic
+resource "aws_lb_target_group" "nginx-tgt" {
+  health_check {
+    interval            = 10
+    path                = "/healthstatus"
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+  name        = "nginx-tgt"
+  port        = 443
+  protocol    = "HTTPS"
+  target_type = "instance"
+  vpc_id      = aws_vpc.main.id
 }
