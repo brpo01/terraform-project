a9f9becb57fb399f5651a2c610a45f46c2dd8932
brpo01 <opraise139@gmail.com>
2021-10-20 21:29:16 +0100

    created the external loadbalancer resource

---
+++ ext-alb.tf
@@ -0,0 +1,22 @@
+resource "aws_lb" "ext-alb" {
+  name     = "ext-alb"
+  internal = false
+  security_groups = [
+    aws_security_group.ext-alb-sg.id,
+  ]
+
+  subnets = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
+
+   tags = merge(
+    var.tags,
+    {
+      Name = "ACS-ext-alb"
+    },
+  )
+
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+}
