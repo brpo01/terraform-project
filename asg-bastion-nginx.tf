d85443ae717797a9b286f7b76c8a1cca6ba8d922
brpo01 <opraise139@gmail.com>
2021-10-21 00:40:06 +0100

    created autoscaling group resource for bastion host

--- asg-bastion-nginx.tf
+++ asg-bastion-nginx.tf
@@ -2,6 +2,8 @@ resource "random_shuffle" "az_list" {
   input        = data.aws_availability_zones.available.names
 }
 
+# ---- Autoscaling for bastion  hosts
+
 resource "aws_launch_template" "bastion-launch-template" {
   image_id               = var.ami
   instance_type          = "t2.micro"
@@ -33,4 +35,31 @@ resource "aws_launch_template" "bastion-launch-template" {
   }
 
   user_data = filebase64("${path.module}/bastion.sh")
+}
+
+# ---- Autoscaling for bastion  hosts
+
+resource "aws_autoscaling_group" "bastion-asg" {
+  name                      = "bastion-asg"
+  max_size                  = 2
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+
+  vpc_zone_identifier = [
+    aws_subnet.public[0].id,
+    aws_subnet.public[1].id
+  ]
+
+  launch_template {
+    id      = aws_launch_template.bastion-launch-template.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "bastion-launch-template"
+    propagate_at_launch =  true
+  }
+
 }
