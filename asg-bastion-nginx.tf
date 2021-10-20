eb7eac832d6f6d3e7cb9cc02133dfbdca95c2e31
brpo01 <opraise139@gmail.com>
2021-10-21 00:41:17 +0100

    created launch template resource for nginx

--- asg-bastion-nginx.tf
+++ asg-bastion-nginx.tf
@@ -63,3 +63,38 @@ resource "aws_autoscaling_group" "bastion-asg" {
   }
 
 }
+
+#--------- launch template for nginx
+
+resource "aws_launch_template" "nginx-launch-template" {
+  image_id               = var.ami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ip.id
+  }
+
+  key_name =  var.keypair
+
+  placement {
+    availability_zone = "random_shuffle.az_list.result"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+    var.tags,
+    {
+      Name = "nginx-launch-template"
+    },
+  )
+  }
+
+  user_data = filebase64("${path.module}/nginx.sh")
+}
