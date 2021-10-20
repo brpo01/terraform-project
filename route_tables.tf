ffbbe7dc7d97c619836e7827a99aa3b8d19a057d
brpo01 <opraise139@gmail.com>
2021-10-20 15:03:46 +0100

    created private route table

---
+++ route_tables.tf
@@ -0,0 +1,11 @@
+# create private route table
+resource "aws_route_table" "private-rtb" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-Private-Route-Table", var.name)
+    },
+  )
+}
