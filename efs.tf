d0260ab35b3278a7ec11387d457490cf8f0ed3dd
brpo01 <opraise139@gmail.com>
2021-10-21 01:04:08 +0100

    created efs file system resource

---
+++ efs.tf
@@ -0,0 +1,12 @@
+# create Elastic file system
+resource "aws_efs_file_system" "ACS-efs" {
+  encrypted  = true
+  kms_key_id = aws_kms_key.ACS-kms.arn
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "ACS-efs"
+    },
+  )
+}
