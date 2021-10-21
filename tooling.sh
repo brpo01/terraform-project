d0c9cfd271e4dd7f7749d25822c38a00c6757b79
brpo01 <opraise139@gmail.com>
2021-10-21 22:33:28 +0100

    added tooling userdata script

--- tooling.sh
+++ tooling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /var/www/
-sudo mount -t efs -o tls,accesspoint=fsap-0f9364679383ffbc0 fs-8b501d3f:/ /var/www/
+sudo mount -t efs -o tls,accesspoint=fsap-01c13a4019ca59dbe fs-8b501d3f:/ /var/www/
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
@@ -9,17 +9,13 @@ yum module enable php:remi-7.4 -y
 yum install -y php php-common php-mbstring php-opcache php-intl php-xml php-gd php-curl php-mysqlnd php-fpm php-json
 systemctl start php-fpm
 systemctl enable php-fpm
-wget http://wordpress.org/latest.tar.gz
-tar xzvf latest.tar.gz
-rm -rf latest.tar.gz
-cp wordpress/wp-config-sample.php wordpress/wp-config.php
-mkdir /var/www/html/
-cp -R /wordpress/* /var/www/html/
+git clone https://github.com/Livingstone95/tooling-1.git
+mkdir /var/www/html
+cp -R /tooling-1/html/*  /var/www/html/
+cd /tooling-1
+mysql -h acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com -u ACSadmin -p toolingdb < tooling-db.sql
 cd /var/www/html/
 touch healthstatus
-sed -i "s/localhost/acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com/g" wp-config.php
-sed -i "s/username_here/ACSadmin/g" wp-config.php
-sed -i "s/password_here/admin12345/g" wp-config.php
-sed -i "s/database_name_here/wordpressdb/g" wp-config.php
+sed -i "s/$db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooling');/$db = mysqli_connect('acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com ', 'ACSadmin', 'admin12345', 'toolingdb');/g" functions.php
 chcon -t httpd_sys_rw_content_t /var/www/html/ -R
 systemctl restart httpd
