764d28659a38ef29d6cbf276938ee7c9b80f2a10
brpo01 <opraise139@gmail.com>
2021-10-21 18:06:52 +0100

    modified rds.tf

--- rds.tf
+++ rds.tf
@@ -1,12 +1,12 @@
 # This section will create the subnet group for the RDS  instance using the private subnet
-resource "aws_db_subnet_group" "ACS-rds" {
-  name       = "acs-rds"
+resource "aws_db_subnet_group" "main-rds" {
+  name       = "main-rds"
   subnet_ids = [aws_subnet.private_subnet[2].id, aws_subnet.private_subnet[3].id]
 
  tags = merge(
     var.tags,
     {
-      Name = "ACS-rds"
+      Name = "main-rds"
     },
   )
 }
@@ -22,7 +22,7 @@ resource "aws_db_instance" "main-rds" {
   username               = var.master-username
   password               = var.master-password
   parameter_group_name   = "default.mysql5.7"
-  db_subnet_group_name   = aws_db_subnet_group.ACS-rds.name
+  db_subnet_group_name   = aws_db_subnet_group.main-rds.name
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.datalayer-sg.id]
   multi_az               = "true"
