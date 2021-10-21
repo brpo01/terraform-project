dae5047e6da5debdd4cf6e02fc18d1c3a096ddef
brpo01 <opraise139@gmail.com>
2021-10-21 01:07:36 +0100

    created access point for wordpress app

--- efs.tf
+++ efs.tf
@@ -24,3 +24,25 @@ resource "aws_efs_mount_target" "subnet-2" {
   subnet_id       = aws_subnet.private[3].id
   security_groups = [aws_security_group.datalayer-sg.id]
 }
+
+# create access point for wordpress
+resource "aws_efs_access_point" "wordpress" {
+  file_system_id = aws_efs_file_system.ACS-efs.id
+
+  posix_user {
+    gid = 0
+    uid = 0
+  }
+
+  root_directory {
+    path = "/wordpress"
+
+    creation_info {
+      owner_gid   = 0
+      owner_uid   = 0
+      permissions = 0755
+    }
+
+  }
+
+}
