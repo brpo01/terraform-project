186d159acc804000f0d09c8e3099bd89f90f7db9
brpo01 <opraise139@gmail.com>
2021-10-23 00:34:32 +0100

    dynamodb resource for state locking consistency checking

--- backends.tf
+++ backends.tf
@@ -14,3 +14,13 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
