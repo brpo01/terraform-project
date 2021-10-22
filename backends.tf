da4a202ab9ffe37077e36aa8aeb526e2cf8af359
brpo01 <opraise139@gmail.com>
2021-10-23 00:27:41 +0100

    created s3 bucket resource

--- backends.tf
+++ backends.tf
@@ -1,26 +1,16 @@
-// resource "aws_s3_bucket" "terraform_state" {
-//   bucket = "toby-s3-bucket"
-//   # Enable versioning so we can see the full revision history of our state files
-//   versioning {
-//     enabled = true
-//   }
-//   # Enable server-side encryption by default
-//   server_side_encryption_configuration {
-//     rule {
-//       apply_server_side_encryption_by_default {
-//         sse_algorithm = "AES256"
-//       }
-//     }
-//   }
-// }
-
-// resource "aws_dynamodb_table" "terraform_locks" {
-//   name         = "toby-s3-dynamodb"
-//   billing_mode = "PAY_PER_REQUEST"
-//   hash_key     = "LockID"
-//   attribute {
-//     name = "LockID"
-//     type = "S"
-//   }
-// }
-
+# Note: The bucket name may not work for you since buckets are unique globally in AWS, so you must give it a unique name.
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "rotimi-dev-terraform-bucket"
+  # Enable versioning so we can see the full revision history of our state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
