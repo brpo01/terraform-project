22e6250ab775e130d501da1247f801ef5f515ab9
brpo01 <opraise139@gmail.com>
2021-10-23 10:40:38 +0100

    changed s3 region

--- backends.tf
+++ backends.tf
@@ -31,7 +31,7 @@ terraform {
   backend "s3" {
     bucket         = "rotimi-dev-terraform-bucket"
     key            = "global/s3/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
