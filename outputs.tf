c69ba56303bf7bee1a9079080be1bc9813c95c70
brpo01 <opraise139@gmail.com>
2021-10-23 10:58:10 +0100

    added outputs for s3 bucket arn & dynamodb table

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,12 @@ output "alb_dns_name" {
 output "alb_target_group_arn" {
   value = aws_lb_target_group.nginx-tgt.arn
 }
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
