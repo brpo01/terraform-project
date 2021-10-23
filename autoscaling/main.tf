ec0fcb47e4fc8a9e17d956f1be058afa57f47e49
brpo01 <opraise139@gmail.com>
2021-10-23 20:40:29 +0100

    refactoring autoscaling module

--- autoscaling/main.tf
+++ autoscaling/main.tf
@@ -1,3 +1,26 @@
+// Creating sns topic for all the auto scaling groups
+resource "aws_sns_topic" "rotimi-sns" {
+    name = "Default_CloudWatch_Alarms_Topic"
+}
+
+// Creating Notifications for all autoscaling groups
+resource "aws_autoscaling_notification" "rotimi_notifications" {
+  group_names = [
+    aws_autoscaling_group.bastion-asg.name,
+    aws_autoscaling_group.nginx-asg.name,
+    aws_autoscaling_group.wordpress-asg.name,
+    aws_autoscaling_group.tooling-asg.name,
+  ]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+  ]
+
+  topic_arn = aws_sns_topic.rotimi-sns.arn
+}
+
 # ---- Autoscaling for bastion  hosts
 
 resource "aws_autoscaling_group" "bastion-asg" {
