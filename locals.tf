ce7e8144301730e92b10445b314e74301485b69a
brpo01 <opraise139@gmail.com>
2021-10-23 17:08:47 +0100

    adding security groups to locals.tf for dynamism

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
     security_group = {
         ext-alb = {
-            name = "External Loadbalancer"
+            name = "ext-alb"
             description = "Security Group for Ext-Alb"
             ingress = {
                 http = {
@@ -21,11 +21,35 @@ locals {
         }
         bastion = {
             name = "Bastion"
-            description = "Security Group Bastion"
+            description = "Bastion Security Group"
             ingress = {
                 ssh = {
                     from = 22
                     to = 22
+                    protocol = "tcp"
+                    cidr_blocks = [0.0.0.0/0]
+                }
+            }
+        }
+        Nginx = {
+            name = "Nginx"
+            description = "Nginx Security Group"
+            ingress = {
+                https = {
+                    from = 443
+                    to = 443
+                    protocol = "tcp"
+                    cidr_blocks = [0.0.0.0/0]
+                }
+            }
+        }
+        int-alb = {
+            name = "int-alb"
+            description = "Internal Loadbalancer Security Group"
+            ingress = {
+                ssh = {
+                    from = 443
+                    to = 443
                     protocol = "tcp"
                     cidr_blocks = [0.0.0.0/0]
                 }
