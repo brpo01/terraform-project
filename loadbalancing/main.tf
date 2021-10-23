fe087c6733569711cf81e21337e1dfe3a0ad65b5
brpo01 <opraise139@gmail.com>
2021-10-23 21:30:44 +0100

    refactoring code

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -113,7 +113,7 @@ resource "aws_lb_target_group" "tooling-tgt" {
   port        = 443
   protocol    = "HTTPS"
   target_type = "instance"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.vpc_id
 }
 
 # For this aspect a single listener was created for the wordpress which is default,
