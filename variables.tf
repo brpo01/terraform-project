84c2ffef513655e2b5fdb852ceb2a3551f53422f
brpo01 <opraise139@gmail.com>
2021-10-31 14:29:50 +0100

    adding default values to root variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "region" {
   type = string
   description = "The region to deploy resources"
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
   type = string
   description = "The VPC cidr"
+  default = "172.16.0.0/16" 
 }
 
 variable "name" {
@@ -22,29 +24,35 @@ variable "tags" {
 variable "ami" {
   type        = string
   description = "AMI ID for the launch template"
+  default = "ami-09e67e426f25ce0d7"
 }
 
 variable "keypair" {
   type        = string
   description = "key pair for the instances"
+  default = "terraform-ec2"
 }
 
 variable "account_no" {
   type        = number
   description = "the account number"
+  default = "323678568700"
 }
 
 variable "master-username" {
   type        = string
   description = "RDS admin username"
+  default = "rotimi"
 }
 
 variable "master-password" {
   type        = string
   description = "RDS master password"
+  default = "devopspblproject"
 }
 
 variable "environment" {
   type = string
+  default = production
 }
 
